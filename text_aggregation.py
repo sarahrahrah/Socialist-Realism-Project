23a64f6e53bd6c47e0f0a8c297f143f616e51bec
Sarah McEleney <sarahmceleney@Sarahs-MBP.hsd1.va.comcast.net>
2018-06-12 11:01:18 -0400

    first steps in upservised learning

---
+++ text_aggregation.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+import pandas as pd
+from collections import OrderedDict
+
+namelist = []
+titlelist = []
+datelist = []
+englishmetadatalist = []
+sourcelist = []
+textlist = []
+
+for dirpath, dirnames, filenames in os.walk("preliminary_corpus_files"):
+    for filename in filenames:
+        if filename != ".DS_Store":
+            z = os.path.join(dirpath, filename)
+            y = open(z, 'rb')
+            print (filename)
+            data = y.read().decode('utf8', 'ignore')
+            name = data.split("***")[0]
+            namelist.append(name)
+            title = data.split("***")[1]
+            titlelist.append(title)
+            date = data.split("***")[2]
+            datelist.append(date)
+            englishmetadata = data.split("***")[3]
+            englishmetadatalist.append(englishmetadata)           
+            source = data.split("***")[4]
+            sourcelist.append(source)
+            text = data.split("***")[5]
+            textlist.append(text)
+
+newdataframe = pd.DataFrame(OrderedDict({
+        
+        "title": titlelist,
+        "name": namelist,
+        "date": datelist,
+        "english metadata" : englishmetadatalist,
+        "source": sourcelist,
+        "text": textlist,
+    }))
+
+newdataframe.to_csv('LITERATURE.csv', index=False)
+
+print ("The LITERATURE.csv file is now created.")
+
+
+
+
+
---
+++ topicmodeling.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords 
+import string
+import gensim
+from gensim import corpora
+import matplotlib.pyplot as plt
+import plotly
+print(plotly.__version__)  # version >1.9.4 required
+from plotly.graph_objs import Scatter, Layout
+import collections
+from collections import OrderedDict
+
+
+ourdata = pd.read_csv("LITERATURE.csv")
+
+thetext = ourdata["text"]
+
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("russian")
+
+stop = set(stopwords.words('russian'))
+
+
+otherstopwords = ["это","да","наш","сво","одн","говор","сказа","нам","—","котор","мо","очен"]
+otherstop = set(otherstopwords)
+
+stop.update(otherstop)
+
+
+
+exclude = set(string.punctuation) 
+
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    stop_freeagain = " ".join([i for i in punc_free.split() if i not in stop])
+    joined = " ".join([stemmer.stem(word) for word in stop_freeagain.split()])
+    stopfree2 = " ".join([i for i in joined.split() if i not in stop])
+    normalized = stopfree2.split()
+    return normalized
+
+
+pd.options.display.max_colwidth = 500
+ourdata["cleantext"] = ourdata["text"].apply(clean)
+
+print (ourdata["cleantext"])
+
+
+dictionary = corpora.Dictionary(ourdata["cleantext"])
+doc_term_matrix = [dictionary.doc2bow(item) for item in ourdata["cleantext"]]
+Lda = gensim.models.ldamodel.LdaModel
+ldamodel = Lda(doc_term_matrix, num_topics=5, id2word = dictionary, passes=50)
+ouroutput = ldamodel.print_topics(num_topics=5, num_words=4)
+
+
+
+print(ouroutput)
+
+listforplot = [list(i) for i in ouroutput]
+print (listforplot)
+
+topicnos = []
+wordsfornew = []
+
+
+for i in listforplot:
+    newlabel = "Topic " + str(i[0] + 1)
+    words = str(i[1])
+    wordsfornew.append(words)
+    topicnos.append(newlabel)
+
+
+
+modelres = OrderedDict([('Topic Number',topicnos),
+                     ('Words', wordsfornew)])
+
+newdf = pd.DataFrame.from_dict(modelres)
+
+print (newdf)
+                     
+
+from plotly import figure_factory as ff
+table = ff.create_table(newdf)
+plotly.offline.plot(table, filename='LITERATURETOPICMODELING.html')
+
+
