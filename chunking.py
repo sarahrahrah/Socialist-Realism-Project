69aa64125610720ddcd374981f7cd738ed6e14d2
Sarah McEleney <sarahmceleney@Sarahs-MBP.hsd1.va.comcast.net>
2018-07-14 10:14:30 -0400

    added a few lines

--- chunking.py
+++ chunking.py
@@ -1,5 +1,6 @@
 
 import nltk
+import math
 
 z = "./preliminary_corpus_files_chunked/banned_lit_corpus/platonov_the-foundation-pit.txt"
 
@@ -18,13 +19,19 @@ print (len(file))
 
 
 tokens = nltk.word_tokenize(file)
+toklen =  len(tokens)
 
-for i in range(0, 8):
+length = toklen / 5000
+rounduplength = math.ceil(length)
+print (len(tokens))
+
+for i in range(0, rounduplength):
     chunk = str(i + 1) 
     if len(tokens) >= 5000:
         tokensarray = tokens[0:5000]
         joinedtokensarray = ' '.join(tokensarray)
         text_file = open("./preliminary_corpus_files_chunked/banned_lit_corpus/platonov_the-foundation-pit-" + chunk + ".txt","w")
+        text_file.write("Платонов, Андрей Платонович *** Котлован " + chunk + "*** 1926-1928 *** Andrei Platonov, The Foundation Pit, 1930 *** https://ilibrary.ru/text/1010/index.html ***")
         text_file.write(joinedtokensarray)
         del tokens[0:5000]
 
@@ -32,11 +39,9 @@ for i in range(0, 8):
         tokensarray = tokens
         joinedtokensarray = ' '.join(tokensarray)
         text_file = open("./preliminary_corpus_files_chunked/banned_lit_corpus/platonov_the-foundation-pit-" + chunk + ".txt","w")
+        text_file.write("Платонов, Андрей Платонович *** Котлован " + chunk + "*** 1926-1928 *** Andrei Platonov, The Foundation Pit, 1930 *** https://ilibrary.ru/text/1010/index.html ***")
         text_file.write(joinedtokensarray)
-
-
-
-    
+print (rounduplength)
         
 #    x = tokens[i]
     
